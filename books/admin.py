ca7c1100a4ff9ccc33d72a4d489aa3346488b53f
MagiMats <matsjanssens2004@gmail.com>
2022-08-12 09:25:16 +0200

    form with admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book
+from .forms import BookForm
 
 # Register your models here.
+
+class BookAdmin(admin.Admin):
+    add_form = BookForm
+    model = Book
+    
+admin.site.register(Book)
+
+
--- books/models.py
+++ books/models.py
@@ -4,8 +4,18 @@ from users.models import CustomUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Book(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    #Make a foreignkey to the user so that they are linked to this book and can only acces it
+
     name = models.CharField(max_length=50)
+    #Name to be displayed of the book
+
     page_amount = models.IntegerField(validators = [MinValueValidator(1)])
+    #Total amount of pages
+
     current_page = models.IntegerField(validators = [MaxValueValidator(page_amount), MinValueValidator(1)])
+    #The page which the user last opened
+
     file = models.FileField(upload_to="media/pdf/")
+    #The link to the actual field where the file is located
+    
