eb5347f1156035bfb55faccbb3328a71ae4ef310
MagiMats <matsjanssens2004@gmail.com>
2022-08-12 09:16:44 +0200

    Book Model filefield test

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import CustomUser
 
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+class Book(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    page_amount = models.IntegerField(validators = [MinValueValidator(1)])
+    current_page = models.IntegerField(validators = [MaxValueValidator(page_amount), MinValueValidator(1)])
+    file = models.FileField(upload_to="media/pdf/")
