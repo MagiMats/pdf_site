51395fe67754c1ec2eb24fe36a237ba77b0fe601
MagiMats <matsjanssens2004@gmail.com>
2022-08-12 09:32:13 +0200

    makemigrations + migrate applied

--- books/admin.py
+++ books/admin.py
@@ -4,10 +4,10 @@ from .forms import BookForm
 
 # Register your models here.
 
-class BookAdmin(admin.Admin):
-    add_form = BookForm
+class BookAdmin(admin.ModelAdmin):
+    form = BookForm
     model = Book
-    
+
 admin.site.register(Book)
 
 
--- pdf_site/settings.py
+++ pdf_site/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+
+    'books'
 ]
 
 MIDDLEWARE = [
